5d8070d34d5b6a574b9cb0fcd8b4790412ab0d18
Abdelhak ZABOUR <abdelhakzabour@gmail.com>
2022-04-24 19:34:48 +0200

    add json credential path

--- ocr.py
+++ ocr.py
@@ -14,7 +14,7 @@ import os
 
 
 # ! Add your credential JASON file path
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../ocr-test-344811-c1e722186220.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="../path_to_credential_json_file.json"
 
 def process_text(text):
     # preprocess the text
